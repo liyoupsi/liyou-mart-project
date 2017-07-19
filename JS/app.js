b8845737b6882750acc8d85bcef4f2828932817a
liwam kid <honny871@yahoo.com>
2017-07-19 18:15:44 -0400

    final project upload

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,10 @@
-var x = document.getElementById("form");
-var createform = document.createElement('form');
-createform.setAttribute("action", "");
-createform.setAttribute("method", "post");
-x.appendChild(createform);
-
-var heading = document.createElement('h2');
-heading.innerHTML = "Contact Form ";
-createform.appendChild(heading);
-
-var line = document.createElement('hr');
-createform.appendChild(line);
-var emaillabel = document.createElement('label');
-emaillabel.innerHTML = "Your Email : ";
-createform.appendChild(emaillabel);
-
-var emailelement = document.createElement('input');
-emailelement.setAttribute("type", "text");
-emailelement.setAttribute("name", "demail");
-createform.appendChild(emailelement);
-var submitelement = document.createElement('input'); // Append Submit Button
-submitelement.setAttribute("type", "submit");
-submitelement.setAttribute("name", "dsubmit");
-submitelement.setAttribute("value", "Submit");
-createform.appendChild(submitelement);
+<script type="text/javascript">
+function submitform()
+{
+    document.forms["myform"].submit();
+}
+</script>
+<form id="myform" action="submit-form.php">
+Email: <input type='text' name='query'>
+<a href="javascript: submitform()">Submit</a>
+</form>
